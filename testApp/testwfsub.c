040ddd67a81a421b1642923e9bf3d1db09d6e06e
Michael Davidsaver <mdavidsaver@gmail.com>
2026-05-25 17:39:42 -0700

    implement and test

--- testApp/testwfsub.c
+++ testApp/testwfsub.c
@@ -1,8 +1,62 @@
 
+#define USE_TYPED_RSET
+
 #include <epicsUnitTest.h>
+#include <dbUnitTest.h>
 #include <testMain.h>
 
+#include <alarm.h>
+#include <dbDefs.h>
+#include <dbAccess.h>
+#include <dbStaticLib.h>
+
+static
+void testCase1()
+{
+    {
+        const double inp[] = {1.0, 10.0, 0.0, -1.0};
+        testdbPutArrFieldOk("tst1:V", DBR_DOUBLE, NELEMENTS(inp), inp);
+    }
+    testdbGetFieldEqual("tst1:sub.SEVR", DBF_LONG, INVALID_ALARM);
+    testdbGetFieldEqual("tst1:sub.AMSG", DBF_STRING, "NaN @2");
+    {
+        const double expect[] = {0.0, 20.0, 1e-30, 1e-30};
+        testdbGetArrFieldEqual("tst1:dB", DBR_DOUBLE,
+                               NELEMENTS(expect)+1,
+                               NELEMENTS(expect), expect);
+    }
+}
+
+static
+void testCase2()
+{
+    {
+        const double inp[] = {1.0, 10.0, 100.0};
+        testdbPutArrFieldOk("tst1:V", DBR_DOUBLE, NELEMENTS(inp), inp);
+    }
+    testdbGetFieldEqual("tst1:sub.SEVR", DBF_LONG, NO_ALARM);
+    testdbGetFieldEqual("tst1:sub.AMSG", DBF_STRING, "");
+    {
+        const double expect[] = {0.0, 20.0, 40};
+        testdbGetArrFieldEqual("tst1:dB", DBR_DOUBLE,
+                               NELEMENTS(expect)+1,
+                               NELEMENTS(expect), expect);
+    }
+}
+
+int testwfsub_registerRecordDeviceDriver(DBBASE *pbase);
+
 MAIN(testwfsub) {
-    testPlan(0);
+    testPlan(8);
+    testdbPrepare();
+    testdbReadDatabase("testwfsub.dbd", NULL, NULL);
+    testwfsub_registerRecordDeviceDriver(pdbbase);
+    testdbReadDatabase("test1.db", NULL, "P=tst1:");
+    testIocInitOk();
+    // "isolated" IOC running
+    testCase1();
+    testCase2();
+    testIocShutdownOk();
+    testdbCleanup();
     return testDone();
 }
--- wfSimApp/src/wfsub.c
+++ wfSimApp/src/wfsub.c
@@ -1,14 +1,60 @@
 
+#include <epicsStdlib.h>
+#include <epicsMath.h>
 #include <alarm.h>
 #include <recGbl.h>
+#include <menuFtype.h>
 #include <aSubRecord.h>
 
 #include <registryFunction.h>
 #include <epicsExport.h>
 
+/* Log scale example (for real, use aCalcoutRecord!!!)
+ *
+ * record(aSub, "$(P)C$(CH):V_") {
+ *     field(SNAM, "wfsimScale")
+ *     field(FTA, "DOUBLE")
+ *     field(NOA , "$(NELM=512)")
+ *     field(INPA, "") # volts
+ *     field(FTVA, "DOUBLE")
+ *     field(NOVA, "$(NELM=512)")
+ *     field(OUTA, "") # dBm
+ * }
+ */
 static
-long wfsimScale(aSubRecord *prec) {
-    recGblSetSevrMsg(prec, COMM_ALARM, INVALID_ALARM, "Nope");
+long wfsimScale(aSubRecord *prec)
+{
+    if(prec->fta != menuFtypeDOUBLE) {
+        recGblSetSevrMsg(prec, COMM_ALARM, INVALID_ALARM, "Wrong FTA");
+        return -1;
+    }
+    if(prec->ftva != menuFtypeDOUBLE) {
+        recGblSetSevrMsg(prec, COMM_ALARM, INVALID_ALARM, "Wrong FTVA");
+        return -1;
+    }
+
+    const double *a = prec->a;
+    const size_t a_count = prec->nea;
+
+    double *vala = prec->vala;
+    const size_t vala_capacity = prec->nova;
+
+    size_t desired = a_count;
+    if(desired > vala_capacity)
+        desired = vala_capacity;
+
+    for(size_t i=0; i<desired; i++) {
+        double e = a[i];
+        if(!isfinite(e) || e <= 0.0) {
+            recGblSetSevrMsg(prec, CALC_ALARM, INVALID_ALARM, "NaN @%zu", i);
+            vala[i] = 1e-30;
+        } else {
+            vala[i] = 20*log10(a[i]);
+        }
+    }
+
+    prec->neva = desired;
+
     return 0;
 }
 
